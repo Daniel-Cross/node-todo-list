f04cc78b5b35bdc1c93c3e790edc85a7f7fecf3c
Daniel Cross <crossy1686@gmail.com>
2019-03-20 23:00:45 +0100

    add fetch request

---
+++ public/app.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', () => {
+	fetch('/api/todos', {
+		method: 'GET',
+	})
+		.then(function(res) {
+			return res.json();
+		})
+		.then(addTodos)
+		.catch(function(err) {
+			console.log(err);
+		});
+});
+
+addTodos = (todos) => {
+	todos.forEach((todo) => {
+		const newTodo = todo.name.split(',');
+		console.log(newTodo);
+		console.log(todo.completed);
+
+		for (name in newTodo) {
+			const newElement = document.createElement('LI');
+
+			if (todo.completed) {
+				newElement.className = 'done';
+			} else {
+				newElement.className = 'task';
+			}
+			newElement.innerHTML = newTodo[name];
+			document.body.appendChild(newElement);
+		}
+	});
+};
